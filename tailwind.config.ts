f6fc7db6bbb5ea2a24b38ea84a978f28639543ae
김재윤 <ppapic4020@gmail.com>
2026-02-26 15:24:47 +0000

    폰트를 프리텐다드로 바꿀 수 있을까?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
+        body: ['Pretendard', 'var(--font-inter)', 'sans-serif'],
         headline: ['var(--font-space-grotesk)', 'sans-serif'],
       },
       colors: {
